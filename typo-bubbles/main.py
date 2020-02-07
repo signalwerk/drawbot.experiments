d1283fdaa81b0566a5c3b1ba9122be94da3d86f7
signalwerk <sh@signalwerk.ch>
2020-02-07 15:23:53 +0100

    MOD: clean up bubble script

--- typo-bubbles/main.py
+++ typo-bubbles/main.py
@@ -2,7 +2,7 @@ from random import choice
 import datetime
 import math
 
-
+# helper functions
 def s(x):
   return x * scaler
 
@@ -15,36 +15,37 @@ def nowStr():
 def sinOffset(t, max):
     return math.sin(math.tau * 1 / max * t)
 
-# canvas setup
+# seutp animatinon 1000px@24 fps
 scaler = 1000;
 fps = 24
+
+# how many bubbles
 bubbleCount = 300
+
 debug = False
 
+# have it reduced in debug mode
 if (debug):
     fps = 1
     bubbleCount = 20
 
-
+# 18 sec animation
 animLength = 18 * fps
+
+# fontSize
 fSize = s(0.1)
 
+# canvas setup
 widht = s(1);
 height = round(s(1 / 16 * 9));
 
-
-
-
-
+# what characters do we need to draw (some appear more often)
 possibleTexts = ["t", "y", "p", "o", "*", ",", ")", "_", ".", "t", "y", "p", "o" ]
 
 
-## bubbles start
-## bubbleX = map(rand, range(bubbleCount))
-## bubbleY = map(rand, range(bubbleCount))
-
+# bubble class
 class Bubble:
-    """A simple example class"""
+    """Bubble as a class"""
     size = 1
     speed = [0, 0]
     age = 0
@@ -56,33 +57,21 @@ class Bubble:
     frequency = 1;
     z = 0
 
-
-    # def __init__(self):
-    #     self.size = rand()
-
+    # get a lifetime tick
     def tick(self):
         self.age = self.age + 1
         if (self.age >= 0 and self.age < self.maxAge) :
             self.x = self.x + self.speed[0]
             self.y = self.y + self.speed[1]
 
+    # draw current state
     def draw(self):
         if (self.age >= 0 and self.age < self.maxAge) :
-
 
             with savedState():
                 translate(self.offset[0],self.offset[1])
 
-                # if debug:
-                #     fill(1,0,0)
-                #     rect(0,0,s(.1),s(1));
-
                 with savedState():
-                    # strokeWidth(s(0.001));
-                    # stroke(0,0,0);
-                    # fill(None);
-                    # oval(0,self.y,self.size,self.size);
-
 
                     stroke(None);
                     fill(0,0,0);
@@ -94,6 +83,8 @@ class Bubble:
                     fontSize(currentFontsize)
 
                     textBox(self.text, (0 - fSize + sinOffsetX, self.y - fSize / 2, fSize * 2, fSize), align="center")
+
+                    # this would be for a Z-Index to have it blured
 
                     # if (round(currentFontsize * 2) > 0):
                     #     im = ImageObject()
@@ -122,31 +113,33 @@ class Bubble:
                     #     image(im, (0 - currentFontsize, self.y - currentFontsize))
 
 
-
-
-
 allBubbles = []
 
 ## setup bubbles
 for i in range(bubbleCount):
 
 
+    # the maximum age a bubble can become
     bubbleMaxAge = animLength / 6
 
-    # bubbleSpeed = (height * .7 + height * .3 * random() )/ bubbleMaxAge
+    # the speed the bubbe rices per life unit
     bubbleSpeed = (height * .7 + height * .3 * random() )/ bubbleMaxAge
 
-
+    # don't generate a bubble too late so it can't disapear before the
+    # animation ends
     maxStart = animLength - bubbleMaxAge
+
+    # create bubble and set the current age and maximum age
     b = Bubble();
     b.age =  0 - (maxStart * random());
     b.maxAge = bubbleMaxAge;
 
-
+    # bigger bubbles have a bigger sidways tension
     frequencyScaler = 0.7 * random() + 0.3
     b.size = 1 - frequencyScaler;
     b.frequency = 7 * frequencyScaler
 
+    # not needed right now see
     b.z = round(random() * s(0.003))
 
     b.speed = [s(0.01) * frequencyScaler, bubbleSpeed];
@@ -156,41 +149,24 @@ for i in range(bubbleCount):
     b.text = choice(possibleTexts)
     allBubbles.append(b)
 
-## drawer
-def bubbles(t):
-    for b in allBubbles:
-        b.draw();
-        b.tick();
 
+# set the state and background
 def gState():
     stroke(None)
     fill(1,1,1)
     rect(0,0,widht,height)
 
-
-
-
-filenameExt = nowStr()
-
+# now run the animation
 for time in range(animLength):
     print(str(time) + " of " + str(animLength))
     newPage(widht, height)
     gState()
     frameDuration(1/fps)
 
-    with savedState():
-        bubbles(time)
+    for b in allBubbles:
+        b.draw();
+        b.tick();
 
-    # saveImage("./typo-bubbles__%d.png" % time)
-    # saveImage('./typo-bubbles' + filenameExt + "___" + str(time) + '.png')
-
-
-
+# only save if we ar not testing...
 if animLength > 20:
     saveImage('./rendering/typo-bubbles' + nowStr() + '.mp4')
-
-
-
-
-
-# print(list(bubbleX))
